f17b41bdb4a294ba424f7b844ca376408163c286
Roman Zaiev <dev@hyphatech.io>
2026-03-04 10:12:42 +0000

    Templates packaging fix

--- src/jailrun/templates.py
+++ src/jailrun/templates.py
@@ -3,13 +3,13 @@ from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader
 
-from jailrun import ROOT_DIR
+from jailrun import PACKAGE_DIR
 
 
 @cache
 def build_jinja_env(templates_dir: Path | None = None) -> Environment:
     jinja_env = Environment(
-        loader=FileSystemLoader(templates_dir or ROOT_DIR / "files"),
+        loader=FileSystemLoader(templates_dir or PACKAGE_DIR / "files"),
         autoescape=True,
     )
     return jinja_env
