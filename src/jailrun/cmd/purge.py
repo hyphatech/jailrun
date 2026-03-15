b2fc46e4e632dfa92aee65c9136a5323c89bdc31
Roman Zaiev <dev@hyphatech.io>
2026-03-15 16:15:10 +0000

    Updated purge command

--- src/jailrun/cmd/purge.py
+++ src/jailrun/cmd/purge.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 from jailrun.cmd.stop import stop_vm
@@ -19,12 +20,18 @@ def _purge(settings: Settings) -> None:
     paths = [
         settings.state_file,
         settings.disk_dir / Path(image_xz).with_suffix(""),
+        settings.disk_dir / "OVMF_VARS.fd",
         settings.cloud_dir / "cloud-init.iso",
+        settings.playbook_cache_dir,
     ]
 
     for p in paths:
         if p.exists():
-            p.unlink()
+            if p.is_dir():
+                shutil.rmtree(p)
+            else:
+                p.unlink()
+
             info(f"Removed {p.name}")
 
     ok("Purge complete.")
